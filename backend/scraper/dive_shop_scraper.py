b21249db7e0d10be4cff9611d95c48ae4361126f
Josh Morris <joshmorris@Joshs-MacBook-Pro.local>
2024-09-12 19:41:17 -0500

    began the scraper for the PADI website

--- backend/scraper/dive_shop_scraper.py
+++ backend/scraper/dive_shop_scraper.py
@@ -7,14 +7,14 @@ import sqlite3
 import string
 
 #-----------------------------------------------------------------------------------------------------------------#
-#SETTING UP DATABASE STUFF
+#SETTING UP DATABASE 
 #Connection and cursor
 conn = sqlite3.connect('diveShop.db')
 cursor = conn.cursor()
 
 #Create Table
 createTable = """CREATE TABLE IF NOT EXISTS
-dive_shop_data(id INTEGER PRIMARY KEY autoincrement, shopName TEXT, shopPhone TEXT, shopEmail TEXT, shopAddress TEXT, shopWebsite TEXT)"""
+dive_shop_data(id INTEGER PRIMARY KEY autoincrement, shopName TEXT, shopPhone TEXT, shopEmail TEXT, shopParish TEXT, shopAddress TEXT, shopWebsite TEXT, activityType TEXT)"""
 cursor.execute(createTable)
 
 #-----------------------------------------------------------------------------------------------------------------#
@@ -23,5 +23,24 @@ chrome_options = Options()
 #chrome_options.add_argument("--headless")
 browser = webdriver.Chrome()
 #-----------------------------------------------------------------------------------------------------------------#
+shop_name = []
+shop_phone = []
+sho_email = []
+shop_address = []
+shop_website = []
+p = 0
 
-browser.get("https://www.padi.com/dive-shops/jamaica/?page=1")
+
+
+for x in range(1,3):
+    browser.get(f"https://www.padi.com/dive-shops/jamaica/?page={x}")
+    p += 1 
+    
+    for x in range(1,21):
+        name = browser.find_element("xpath", 
+                                    f"""/html/body/section/div[3]/div[1]/div[2]/div/div[3]/a[{x}]/p[2]""")
+        name = name.text
+        print(name)
+
+        
+        
